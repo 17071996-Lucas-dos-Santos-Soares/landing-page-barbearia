4936ebf8f432de22845b0aa6ea93d030c3441ac0
Lucas dos Santos Soares <129183068+17071996-Lucas-dos-Santos-Soares@users.noreply.github.com>
2025-08-05 11:29:21 -0300

    atualizando o js da landing page

--- js/app.js
+++ js/app.js
@@ -1,22 +1,27 @@
 window.revelar = ScrollReveal({reset:true})
-
 revelar.reveal('.title-1',{
     durantion: 2000,
     distance: '90px',
     delay: 500,
     origin: 'left'
 })
-
 revelar.reveal('.paragraph-1',{
     durantion: 2000,
     distance: '90px',
     delay:500,
     origin: 'left'
 })
-
 revelar.reveal('.btn-1',{
     durantion: 2000,
     delay: 500,
     origin: 'left',
     distance: '90px'
 })
+const cliente = [
+    {
+        nome: "Wlely",
+        email: "wesleygomesguitar@gmail.com",
+        telefone: '(11)98307-1784',
+        endereço: ': R. Érico Neves de Melo, 15b - Vila Miriam'
+    }
+]
