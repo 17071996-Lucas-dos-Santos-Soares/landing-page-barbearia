58bb9f618ed0a50995e9c6cb770ce3f4306d06e2
Lucas dos Santos Soares <129183068+17071996-Lucas-dos-Santos-Soares@users.noreply.github.com>
2025-08-05 11:10:17 -0300

    Atualizando o js da landing page
    Atualizando o js da landing page para a barbearia

---
+++ js/app.js
@@ -0,0 +1,22 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.title-1',{
+    durantion: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.paragraph-1',{
+    durantion: 2000,
+    distance: '90px',
+    delay:500,
+    origin: 'left'
+})
+
+revelar.reveal('.btn-1',{
+    durantion: 2000,
+    delay: 500,
+    origin: 'left',
+    distance: '90px'
+})
